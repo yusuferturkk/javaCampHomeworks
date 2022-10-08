7ff2776ffa9aac1b5e82dad8e01407ec011f1885
Yusuf Ertürk <80910013+yusuferturkk@users.noreply.github.com>
2022-10-08 20:34:01 +0300

    Java Camp Day-3 Homework-2

---
+++ abstractClasses/src/GamerCalculator.java
@@ -0,0 +1,7 @@
+public abstract class GamerCalculator {
+    public abstract void hesapla();
+
+    public final void gameOver(){
+        System.out.println("Oyun Bitti!");
+    }
+}
--- polymorphismDemo/src/Main.java
+++ polymorphismDemo/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        BaseLogger[] loggers = new BaseLogger[] {new FileLogger(), new DatabaseLogger(), new EmailLogger()};
-
-        for (BaseLogger logger: loggers){
-            logger.log("Log mesajı");
-        }
+//        BaseLogger[] loggers = new BaseLogger[] {new FileLogger(), new DatabaseLogger(), new EmailLogger()};
+//
+//        for (BaseLogger logger: loggers){
+//            logger.log("Log mesajı");
+//        }
 
         CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
         customerManager.add();
